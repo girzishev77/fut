ba7f847956e0252c52d6a15cc086e2e312dde68a
Adam Girzishev <girzishev99@bk.ru>
2023-02-06 01:18:28 +0300

    fut
    fut

---
+++ js/main.js
@@ -0,0 +1,74 @@
+/*let arr = ["lorem", "html", "css", "js", "react"];
+let arr2 = arr.filter((item, index) => {
+  return index >= 3;
+});
+console.log(arr);
+console.log(arr2);*/
+
+/*let num = ["lorem", "html", "css", "js", "react"];
+num.map((item, index) => {
+  console.log(item + " expo");
+});*/
+
+/*let a = [
+  "lorem ipsum",
+  "javascript",
+  "php",
+  "css",
+  "react",
+  "git",
+  "html",
+  "mysql",
+];*/
+/*let b = a.filter((item, index) => {
+  return item.length <= 4;
+});
+console.log(b);*/
+
+/*let arr = [1, -3, 5, 6, -7, 8, 9, -11];
+let arr2 = arr.filter((item, index) => {
+  return item < -1;
+});
+console.log(arr2);*/
+
+/*let map = [10, 2762, 0.42, 192, 3, 28, 1125, 2018, 34, 66, 100];
+let map2 = map.filter((item, index) => {
+  return item % 2 === 0;
+});
+console.log(map2);*/
+
+/*let app = [
+  "lorem ipsum",
+  "javascript",
+  "php",
+  "css",
+  "react",
+  "git",
+  "html",
+  "mysql",
+];
+
+
+
+
+
+
+let app2 = app.filter((item, index) => {
+  return item.length > 3;
+});
+console.log(app2);*/
+/*let app = [5, 6, 7, 8, 9];
+app2 = app.map((item, index) => {
+  return item ** 2;
+});
+console.log(app2);*/
+
+//let acc = [-13, 0, 12, 1662, -0.32, -102, -2];
+//let acc2 = acc.filter((item, index) => {
+//  return item < -1;
+//});
+//console.log(acc2);
+//let result = acc2.reduce((sum, item, index, arr) => {
+//  console.log(sum + item);
+//}, 0);
+//console.log(result);
